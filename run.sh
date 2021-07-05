d84735f25197d7f58dbd788559492ac06a884f2d
谢元青 <markxie@tungee.com>
2021-07-05 15:52:24 +0800

    refacotr: adjust run.sh

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ tar -xf oh-my-zsh.tar.gz .oh-my-zsh
 
 mv .oh-my-zsh ~
 
-chsh -s /bin/zsh
-
 cp .zshrc ~/.zshrc
 
 sed -i '1i\ZSH_DISABLE_COMPFIX=true' .zshrc
+
+chsh -s /bin/zsh
 
 source ~/.zshrc
